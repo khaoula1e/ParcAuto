0d4473e89295b610c274037a4116143c2ea40498
KhaoulaElfatimi <93163622+khaoula1e@users.noreply.github.com>
2022-08-15 13:50:00 +0100

    url

--- client/src/components/Slider/data.js
+++ client/src/components/Slider/data.js
@@ -26,7 +26,7 @@ const people = [
     image:
       driver,
     name: 'Gestion des chauffeurs',
-    url: '/chauffeurs',
+    url: '/driver',
     quote:
             "L'application permettra au gestionnaire du parc automobile d'avoir un aperçu sur les ordres de mission de chaque jour,chose qui lui facilitera la gestion des véhicules disponibles dans le parc",
   },
